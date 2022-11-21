593e0e0096175b62b70e489b7f1faae3bfc56420
ReaGol <reutgoldin@gmail.com>
2022-11-21 22:03:49 +0200

    header with css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import "../header.css";
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className='header'>
+      <nav>
+        <ul>
+          <li>
+            <h1>new game button</h1>
+          </li>
+          <li>
+           <h1>back button</h1>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
