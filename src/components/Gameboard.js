0b51fa3bbd8ae0373ab65facfd2d6e56621687ab
ashgolan <ashgolan@hotmail.com>
2022-11-22 02:13:53 +0200

    sounds and wins

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,5 +1,7 @@
 import "./Gameboard.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
+// import meauAudio from "audio/meau.mp3";
+
 export function Gameboard(props) {
   const [cardMatch, setCardMatch] = useState({
     card1Status: null,
@@ -10,11 +12,11 @@ export function Gameboard(props) {
   });
   const [wrongAnswer, setWrongAnswer] = useState(0);
   const [rightAnswer, setRightAnswer] = useState(0);
-
+  const meau = useRef();
+  const win = useRef();
   const clickHandler = (e) => {
     if (!e.target.classList.contains("backColor")) return;
     e.target.classList.remove("backColor");
-
     let card2;
     if (!cardMatch.card1Status) {
       setCardMatch((prev) => {
@@ -32,8 +34,8 @@ export function Gameboard(props) {
       if (targetOfCard.target1.target.className === card2.className) {
         targetOfCard.target1.target.classList.remove("backColor");
         card2.classList.remove("backColor");
-
         setRightAnswer((prev) => prev + 1);
+        meau.current.play();
       } else {
         setTimeout(() => {
           targetOfCard.target1.target.classList.add("backColor");
@@ -46,26 +48,39 @@ export function Gameboard(props) {
         setCardMatch((prev) => {
           return { card1Status: null, card2Status: null };
         });
-      }, 1000);
+      }, 500);
       props.loading((prev) => !prev);
     }
   };
   return (
     <div className="Board">
-      {rightAnswer === props.randomCards.length / 2 && <h1>🏆</h1>}
+      {rightAnswer === props.randomCards.length / 2 && (
+        <>
+          <img
+            style={{ width: "350px", margin: "auto" }}
+            src="images/done.png"
+          ></img>
+          <audio autoPlay ref={win} src="/audio/win.mp3"></audio>
+        </>
+      )}
       <h4>wrong answers :{wrongAnswer}</h4>
-      <div className="card-grid">
-        {props.randomCards.map((card, index) => (
-          <div key={`card${index}`}>
-            <div
-              onClick={(e) => {
-                clickHandler(e);
-              }}
-              className={`${card} backColor`}
-            ></div>
-          </div>
-        ))}
-      </div>
+
+      <audio ref={meau} src="/audio/meau.mp3"></audio>
+
+      {rightAnswer !== props.randomCards.length / 2 && (
+        <div className="card-grid">
+          {props.randomCards.map((card, index) => (
+            <div key={`card${index}`}>
+              <div
+                onClick={(e) => {
+                  clickHandler(e);
+                }}
+                className={`${card} backColor`}
+              ></div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
